9edf9d4b6815bdae4b07fea492a6328ee2b87eb9
benjamin kisenge <benkisenge03@gmail.com>
2022-09-08 22:17:48 +0200

    Import calcutate module in calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,38 +1,53 @@
-import React, { useState } from 'react';
+import React from 'react';
+import calculate from '../logic/calculate';
 import './calculator.css';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
-  render() {
-    const [state, setState] = useState ({
-      total : null,
-      next : null,
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
       operation: null,
-    })
+    };
+    this.dispspalyValue = this.dispspalyValue.bind(this);
+  }
+
+  dispspalyValue (e) {
+    const btnName = e.target.innerText;
+    const obj = calculate(this.state,btnName);
+    console.log(obj, this.state)
+
+  }
+
+  render() {
+    const {total , next, op} = this.state;
+    console.log(this.state)
     return (
       <div className="container">
         <div className="calc-content">
-          <div className="calc-screen">10</div>
+          <div className="calc-screen">{next}</div>
           <div className="calc-btns">
-            <button type="button">AC</button>
-            <button type="button">%</button>
-            <button type="button">+/-</button>
-            <button type="button" className="signs">&divide;</button>
-            <button type="button">7</button>
-            <button type="button">8</button>
-            <button type="button">9</button>
-            <button type="button" className="signs">x</button>
-            <button type="button">4</button>
-            <button type="button">5</button>
-            <button type="button">6</button>
-            <button type="button" className="signs">-</button>
-            <button type="button">1</button>
-            <button type="button">2</button>
-            <button type="button">3</button>
-            <button type="button" className="signs">+</button>
-            <button type="button" className="item0">0</button>
-            <button type="button">.</button>
-            <button type="button" className="signs">=</button>
+            <button onClick={this.dispspalyValue} type="button">AC</button>
+            <button onClick={this.dispspalyValue} type="button">%</button>
+            <button onClick={this.dispspalyValue} type="button">+/-</button>
+            <button onClick={this.dispspalyValue} type="button" className="signs">&divide;</button>
+            <button onClick={this.dispspalyValue} type="button">7</button>
+            <button onClick={this.dispspalyValue} type="button">8</button>
+            <button onClick={this.dispspalyValue} type="button">9</button>
+            <button onClick={this.dispspalyValue} type="button" className="signs">x</button>
+            <button onClick={this.dispspalyValue} type="button">4</button>
+            <button onClick={this.dispspalyValue} type="button">5</button>
+            <button onClick={this.dispspalyValue} type="button">6</button>
+            <button onClick={this.dispspalyValue} type="button" className="signs">-</button>
+            <button onClick={this.dispspalyValue} type="button">1</button>
+            <button onClick={this.dispspalyValue} type="button">2</button>
+            <button onClick={this.dispspalyValue} type="button">3</button>
+            <button onClick={this.dispspalyValue} type="button" className="signs">+</button>
+            <button onClick={this.dispspalyValue} type="button" className="item0">0</button>
+            <button onClick={this.dispspalyValue} type="button">.</button>
+            <button onClick={this.dispspalyValue} type="button" className="signs">=</button>
           </div>
         </div>
       </div>
--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,117 +1,108 @@
-import React from 'react';
-import Operate from './operate';
+import operate from './operate';
 
 const isNumber = (item) => !!item.match(/[0-9]+/);
 
-class Calculate extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+const calculate = (obj, btnName) => {
+  // const [obj ,btnName] = this.state;
+  if (btnName === 'AC') {
+    return {
+      total: null,
+      next: null,
+      operation: null,
     };
   }
+  if (isNumber(btnName)) {
+    if (btnName === '0' && obj.next === '0') return {};
 
-  render() {
-    const [obj ,btnName] = this.state;
-    if (btnName === 'AC') {
-        return {
-            total: null,
-            next: null,
-            operation : null,
-        }
-    }
-    if(isNumber(btnName)){
-        if(btnName === '0' && obj.next === '0') return {};
-
-        if(obj.operation){
-            if(obj.next && obj.next !=='0'){
-                return { ...obj, next: obj.next + btnName }
-            }
-            return { ...obj, next: btnName };
-        }
-
-        if(obj.next && obj.next !== '0') {
-            return {
-                next : obj.next + btnName,
-                total : null,
-            };
-        }
-        return {
-            next : btnName,
-            total : null,
-        };
-    }
-
-    if (btnName === '.') {
-        if (obj.next) {
-          if (obj.next.includes('.')) {
-            return { ...obj };
-          }
-          return { ...obj, next: `${obj.next}.` };
-        }
-        if (obj.operation) {
-          return { ...obj, next: '0.' };
-        }
-        if (obj.total) {
-          if (obj.total.includes('.')) {
-            return {};
-          }
-          return { ...obj, next: `${obj.total}.` };
-        }
-        return { ...obj, next: '0.' };
-      }
-
-      if (btnName === '=') {
-        if (obj.next && obj.operation) {
-          return {
-            total: Operate(obj.total, obj.next, obj.operation),
-            next: null,
-            operation: null,
-          };
-        }
-        // '=' with no operation, nothing to do
-        return {};
-      }
-
-    if (btnName === '+/-') {
-        if (obj.next) {
-          return { ...obj, next: (-1 * parseFloat(obj.next)).toString() };
-        }
-        if (obj.total) {
-          return { ...obj, total: (-1 * parseFloat(obj.total)).toString() };
-        }
-        return {};
-    }
-
-    if (!obj.next && obj.total && !obj.operation) {
-        return { ...obj, operation: btnName };
-    }
-
-      // User pressed an operation button and there is an existing operation
     if (obj.operation) {
-        if (obj.total && !obj.next) {
-          return { ...obj, operation: btnName };
-        }
-
-        if (!obj.total) {
-          return { total: 0, operation: btnName };
-        }
-
-        return {
-          total: Operate(obj.total, obj.next, obj.operation),
-          next: null,
-          operation: btnName,
-        };
+      if (obj.next && obj.next !== '0') {
+        return { ...obj, next: obj.next + btnName };
+      }
+      return { ...obj, next: btnName };
     }
 
-    if (!obj.next) {
-        return { operation: btnName };
+    if (obj.next && obj.next !== '0') {
+      return {
+        next: obj.next + btnName,
+        total: null,
+      };
     }
     return {
-        total: obj.next,
-        next: null,
-        operation: btnName,
+      next: btnName,
+      total: null,
     };
   }
-}
 
-export default Calculate;
+  if (btnName === '.') {
+    if (obj.next) {
+      if (obj.next.includes('.')) {
+        return { ...obj };
+      }
+      return { ...obj, next: `${obj.next}.` };
+    }
+    if (obj.operation) {
+      return { ...obj, next: '0.' };
+    }
+    if (obj.total) {
+      if (obj.total.includes('.')) {
+        return {};
+      }
+      return { ...obj, next: `${obj.total}.` };
+    }
+    return { ...obj, next: '0.' };
+  }
+
+  if (btnName === '=') {
+    if (obj.next && obj.operation) {
+      return {
+        total: operate(obj.total, obj.next, obj.operation),
+        next: null,
+        operation: null,
+      };
+    }
+    // '=' with no operation, nothing to do
+    return {};
+  }
+
+  if (btnName === '+/-') {
+    if (obj.next) {
+      return { ...obj, next: (-1 * parseFloat(obj.next)).toString() };
+    }
+    if (obj.total) {
+      return { ...obj, total: (-1 * parseFloat(obj.total)).toString() };
+    }
+    return {};
+  }
+
+  if (!obj.next && obj.total && !obj.operation) {
+    return { ...obj, operation: btnName };
+  }
+
+  // User pressed an operation button and there is an existing operation
+  if (obj.operation) {
+    if (obj.total && !obj.next) {
+      return { ...obj, operation: btnName };
+    }
+
+    if (!obj.total) {
+      return { total: 0, operation: btnName };
+    }
+
+    return {
+      total: operate(obj.total, obj.next, obj.operation),
+      next: null,
+      operation: btnName,
+    };
+  }
+
+  if (!obj.next) {
+    return { operation: btnName };
+  }
+  return {
+    total: obj.next,
+    next: null,
+    operation: btnName,
+  };
+};
+
+export default calculate;
