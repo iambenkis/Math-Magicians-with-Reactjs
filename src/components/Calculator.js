1393c9e88c3acd32cfd48409bdfeacb3f225a530
benjamin kisenge <benkisenge03@gmail.com>
2022-09-08 12:17:38 +0200

    Create buttons

---
+++ src/components/Calculator.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function Calculator() {
+  return (
+    <div className="container">
+      <div className="content-calc">
+        <div className="calc-screen" />
+        <div className="calc-btns">
+          <button type="button">AC</button>
+          <button type="button">%</button>
+          <button type="button">+/-</button>
+          <button type="button">&divide;</button>
+          <button type="button">7</button>
+          <button type="button">8</button>
+          <button type="button">9</button>
+          <button type="button">x</button>
+          <button type="button">4</button>
+          <button type="button">5</button>
+          <button type="button">6</button>
+          <button type="button">-</button>
+          <button type="button">1</button>
+          <button type="button">2</button>
+          <button type="button">2</button>
+          <button type="button">3</button>
+          <button type="button">+</button>
+          <button type="button">0</button>
+          <button type="button">.</button>
+          <button type="button">=</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
