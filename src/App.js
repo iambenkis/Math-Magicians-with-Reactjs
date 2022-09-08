d9c3c6691db770d2b9b61e625e3025ce9e500f71
benjamin kisenge <benkisenge03@gmail.com>
2022-09-08 15:33:14 +0200

    Install big.js package

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Calculator from './components/Calculator';
+import Calculator from './logic/Calculator';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
