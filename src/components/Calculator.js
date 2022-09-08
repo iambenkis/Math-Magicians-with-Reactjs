a07c81928e379adbbd3228e578ad6e07b7461c32
benjamin kisenge <benkisenge03@gmail.com>
2022-09-08 13:11:45 +0200

    Update UI colors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -10,22 +10,22 @@ function Calculator() {
           <button type="button">AC</button>
           <button type="button">%</button>
           <button type="button">+/-</button>
-          <button type="button">&divide;</button>
+          <button type="button" className="signs">&divide;</button>
           <button type="button">7</button>
           <button type="button">8</button>
           <button type="button">9</button>
-          <button type="button">x</button>
+          <button type="button" className="signs">x</button>
           <button type="button">4</button>
           <button type="button">5</button>
           <button type="button">6</button>
-          <button type="button">-</button>
+          <button type="button" className="signs">-</button>
           <button type="button">1</button>
           <button type="button">2</button>
           <button type="button">3</button>
-          <button type="button">+</button>
+          <button type="button" className="signs">+</button>
           <button type="button" className="item0">0</button>
           <button type="button">.</button>
-          <button type="button">=</button>
+          <button type="button" className="signs">=</button>
         </div>
       </div>
     </div>
