6ad0ecad8747e7633c009005f82b76bfc852328c
benjamin kisenge <benkisenge03@gmail.com>
2022-09-13 23:55:19 +0200

    Style home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Calculator from './components/Calculator';
 import Header from './components/header';
-
+import Home from './components/home';
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
   render() {
     return (
       <>
         <Header />
-        <Calculator />
+        <Home />
       </>
     );
   }
