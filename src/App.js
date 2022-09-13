e6e8da0ec9e476cdbdfb149276fb762ba948713f
benjamin kisenge <benkisenge03@gmail.com>
2022-09-14 00:19:43 +0200

    Install React routerDom package

--- src/App.js
+++ src/App.js
@@ -5,19 +5,17 @@ import Header from './components/header';
 import Home from './components/home';
 import Quote from './components/quote';
 // eslint-disable-next-line react/prefer-stateless-function
-class App extends React.Component {
-  render() {
+const App = () => {
     return (
-      <>
+      <div>
         <Header />
         <Routes>
             <Route path='/' exact element={<Home />} />
             <Route path='/calculator' exact element={<Calculator />} />
             <Route path='/quote' exact element={<Quote />} />
         </Routes>
-      </>
+      </div>
     );
-  }
 }
 
 export default App;
