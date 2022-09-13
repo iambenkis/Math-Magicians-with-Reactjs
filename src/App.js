8a867cc8fdb0aba2e0c65ffe9b0e8b46285f4e9a
benjamin kisenge <benkisenge03@gmail.com>
2022-09-14 00:05:16 +0200

    Create quote component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Calculator from './components/Calculator';
 import Header from './components/header';
 import Home from './components/home';
+import Quote from './components/quote';
 // eslint-disable-next-line react/prefer-stateless-function
 class App extends React.Component {
   render() {
     return (
       <>
         <Header />
-        <Home />
+        <Quote />
       </>
     );
   }
